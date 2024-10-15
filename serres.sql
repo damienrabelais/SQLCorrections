950c13c53239a0a7c76c16b61bb317713f9f99b5
damienrabelais <damien.boucard@ac-rennes.fr>
2024-10-15 17:17:50 +0200

    Correction TP Serres - FIN

--- serres.sql
+++ serres.sql
@@ -78,3 +78,59 @@ inner join emplacement e on (s.noserre= e.noserre)
 inner join plante p on (e.noplante= p.noplante)
 where nomserre ='Exoticus'
 
+-- 15
+
+select count(*) as "Nombre de plantes européennes"
+from plante p
+inner join region r on (p.noregion = r.noregion)
+where nomregion = 'Europe'
+
+-- 16
+select p.noregion, nomregion, count(*)
+from plante p
+inner join region r on (p.noregion = r.noregion)
+group by p.noregion, nomregion
+having count(*) >= 2
+
+--17
+select avg(frequence)
+from serre s
+inner join vaporisation v on (s.noserre = v.noserre)
+where nomserre = 'Georges Bouvet'
+
+--18
+
+select v.noserre, nomserre, avg(frequence)
+from serre s
+inner join vaporisation v on (s.noserre = v.noserre)
+group by v.noserre, nomserre
+
+--19
+select v.noserre, nomserre, avg(frequence)
+from serre s
+inner join vaporisation v on (s.noserre = v.noserre)
+group by v.noserre, nomserre
+having avg(frequence) > 13
+
+--20
+select nomplante
+from plante
+where noplante not in 
+(select p.noplante
+from serre s
+inner join emplacement e on (s.noserre = e.noserre)
+inner join plante p on (e.noplante = p.noplante)
+where nomserre = 'Exoticus'
+)
+
+--21
+select r.noregion, nomregion, count(noplante)
+from region r
+left outer join plante p on (r.noregion = p.noregion)
+group by r.noregion, nomregion 
+
+--22
+SELECT SERRE.noserre as "n°Serre", nomserre as "Nom Serre", COUNT(noplante) as Nombre
+FROM SERRE 
+LEFT JOIN EMPLACEMENT ON (SERRE.noserre = EMPLACEMENT.noserre)
+GROUP BY SERRE.noserre, nomserre
